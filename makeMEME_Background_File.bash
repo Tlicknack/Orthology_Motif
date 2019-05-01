21c7fff3e450aab006e59971ee9f7519f4b908ca
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2019-05-01 14:35:45 -0700

    Update makeMEME_Background_File.bash
    add 'if' statements to pick up where it left off if you get kicked off server for passing allotted time

--- makeMEME_Background_File.bash
+++ makeMEME_Background_File.bash
@@ -4,16 +4,18 @@
 #PBS -m abe
 #PBS -l nodes=1:ppn=4,walltime=0:10:00,vmem=5gb
 
-#Plan: Create background file for every fasta file of upstream regions
+#Plan: Create background file for every fasta file of upstream regions... job was killed after WGD12016_promoter.fasta
 
 input_dir="/N/dc2/scratch/tlicknac/Newest_All-Aurelias_Upstream"        
 
 for filename in $input_dir/*.fasta; do
-	mv $filename ../
-	var=`basename $filename | sed -r 's/_promoter.fasta//'`
-	var2="${var}_background.fasta"
-	var3=`basename $var2`
-	cat *.fasta > "$var3"
-	mv "../$(basename ${filename})" ./
-	mv $var3 ../test_Background/
+        #if [[ $filename == *[1-9][2-9][0-9][0-9][0-9]* ]]; then
+                mv $filename ../
+                var=`basename $filename | sed -r 's/_promoter.fasta//'`
+                var2="${var}_background.fasta"
+                var3=`basename $var2`
+                cat *.fasta > "$var3"
+                mv "../$(basename ${filename})" ./
+                mv $var3 ../Newest_All-Aurelias_Background_Upstream/
+        #fi
 done
